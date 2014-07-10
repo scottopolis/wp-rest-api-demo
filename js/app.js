da25b9dba6f724870fc6ac5e8f5b8ac3506608c5
Scott Bolinger <designbyscott@gmail.com>
2014-07-10 14:15:59 -0700

    Added page transitions
    Added: pageslider.js and css, hamburger image and css
    In app.js: app.getSinglePost(), app.route(), pageslider integration,
    hash routing

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 var app = {
 
 	init: function() {
-		app.getPosts();
+		console.log('app.init');
+		var hash = window.location.hash.replace(/^.*?#/,'');
+
+		if (hash == '') {
+			app.getPosts();
+		}
+
 	},
 
 	getPosts: function() {
-
+		console.log('app.getPosts');
 		var rootURL = 'http://www.wp4.dev/wp-json';
 
 		$.ajax({
@@ -15,9 +21,9 @@ var app = {
 			success: function(data){
 				
 				$.each(data, function(index, value) {
-					console.log(value.featured_image);
 			      $('ul.topcoat-list').append('<li class="topcoat-list__item">' +
-			      	'<img src="'+value.featured_image.attachment_meta.sizes.medium.url+'" /><br>' +
+			      	'<a class="view-link" href="#'+value.ID+'">' +
+			      	'<img src="'+value.featured_image.attachment_meta.sizes.medium.url+'" /></a><br>' +
 			      	'<h3>'+value.title+'</h3>' +
 			      	'<p>'+value.excerpt+'</p></li>');
 			    });
@@ -28,6 +34,60 @@ var app = {
 
 		});
 
+	},
+
+	getSinglePost: function(postID) {
+		console.log('getSinglePost');
+		var rootURL = 'http://www.wp4.dev/wp-json';
+
+		$.ajax({
+			type: 'GET',
+			url: rootURL + '/posts/' + postID,
+			dataType: 'json',
+			success: function(data){
+				console.log(data);
+				$('.single-post .title').append(data.title);
+				$('.single-post .content').append(data.content);
+
+			},
+			error: function(error){
+				console.log(error);
+			}
+
+		});
+
+	},
+
+	route: function(event) {
+		var homePage =
+    		'<div><ul class="topcoat-list"></ul></div>';
+
+		var singlePost =
+		    '<div><article class="single-post">' +
+		    '<a class="topcoat-button" href="#">Back</a>' +
+		    '<h2 class="title"></h2>' +
+		    '<div class="content"></div>' +
+		    '</article></div>';
+
+		var page,
+        hash = window.location.hash.replace(/^.*?#/,'');
+        console.log(hash);
+
+        if (hash != '') {
+        	page = singlePost;
+        	app.getSinglePost(hash);
+        } else {
+    		page = homePage;
+    		app.init();
+    	}
+
+    	slider.slidePage($(page));
 	}
 
 }
+
+var slider = new PageSlider($("#container"));
+
+$(window).on('hashchange', app.route);
+
+app.route();
